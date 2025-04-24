28424178762a8172a167518af1e613c2471ccc9b
Reza <rezasatria289jakarta@gmail.com>
2025-04-24 19:23:28 +0700

    update-supebase

--- admin.js
+++ admin.js
@@ -1,6 +1,6 @@
 // Inisialisasi Supabase
-const supabaseUrl = 'https://your-project-id.supabase.co'; // GANTI DENGAN URL ANDA
-const supabaseKey = 'your-anon-key'; // GANTI DENGAN ANON KEY ANDA
+const supabaseUrl = 'https://znehlqzprtwvhscoeoim.supabase.co'; // GANTI DENGAN URL ANDA
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpuZWhscXpwcnR3dmhzY29lb2ltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0MjQzNzUsImV4cCI6MjA2MTAwMDM3NX0.XsjXAE-mt7RMIncAJuO6XSdZxhQQv79uCUPPVU9mF2A'; // GANTI DENGAN ANON KEY ANDA
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 // DOM Elements
