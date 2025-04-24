c6ba7d750bcae6dad674277c426784ecc9511327
Reza <rezasatria289jakarta@gmail.com>
2025-04-24 22:12:47 +0700

    update-supabase

--- supabase.js
+++ supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
+const supabaseUrl = "https://znehlqzprtwvhscoeoim.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpuZWhscXpwcnR3dmhzY29lb2ltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0MjQzNzUsImV4cCI6MjA2MTAwMDM3NX0.XsjXAE-mt7RMIncAJuO6XSdZxhQQv79uCUPPVU9mF2A"
 export const supabase = createClient(supabaseUrl, supabaseKey)
